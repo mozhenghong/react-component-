0afbfb06587a930387f6a1d7316d327c8f69fd2f
mozhenghong <1445107381@qq.com>
2020-04-02 17:26:38 +0800

    测试classes

---
+++ lib/helpers/__tests__/classes.unit.jsx
@@ -0,0 +1,24 @@
+import classes from '../classes'
+
+describe("classes", () => {
+    it('接受一个className', () => {
+      const result = classes('a')
+      expect(result).toEqual('a')
+    })
+    it('接受两个className', () => {
+        const result = classes('a', 'b')
+        expect(result).toEqual('a b')
+    })
+    it('接受undefined不会出现undefined', () => {
+        const result = classes('a', undefined)
+        expect(result).toEqual('a')
+    })
+    it('接受各种奇怪值不会返回', () => {
+        const result = classes('a', '啦', null, false)
+        expect(result).toEqual('a 啦')
+    })
+    it('接受0个参数', () => {
+        const result = classes()
+        expect(result).toEqual('')
+    })
+})
