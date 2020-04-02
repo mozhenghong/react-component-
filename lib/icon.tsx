fe6179daa0a45d5d633e69bd6a6f570d1b10f2f4
mozhenghong <1445107381@qq.com>
2020-04-02 15:15:50 +0800

    添加onClick事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './importAllIcons';
 import './icon.scss'
 interface IconProps {
-    name:string
+    name: string;
+    onClick: React.MouseEventHandler<SVGElement>
 }
 const Icon: React.FunctionComponent<IconProps> = (props) => {
     return (
-        <svg className="reactComponent-icon">
+        <svg className="reactComponent-icon" onClick={props.onClick}>
             <use xlinkHref={`#${props.name}`}/>
         </svg>
     )
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Icon from './icon';
 
+const fn: React.MouseEventHandler = (e) => {
+    console.log(e.target)
+}
 ReactDom.render(
     <div>
-        <Icon name="wechat"/>
-        <Icon name="QQ"/>
-        <Icon name="alipay"/>
+        <Icon name="wechat" onClick={fn}/>
+        {/* <Icon name="QQ"/>
+        <Icon name="alipay"/> */}
     </div>
     , document.querySelector("#root"))
