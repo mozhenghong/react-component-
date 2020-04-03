51bb341894a6a0efc968169c439ae53c42fc123a
mozhenghong <1445107381@qq.com>
2020-04-03 14:55:26 +0800

    修改package.json

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,14 +1,3 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import Icon from './icon/icon'
+import Icon from './icon/icon';
 
-const fn: React.MouseEventHandler = (e) => {
-    console.log(e.target)
-}
-ReactDom.render(
-    <div>
-        <Icon name="wechat" onClick={fn} onTouchStart={() => console.log('touch')} className="xxx" />
-        <Icon name="QQ" onMouseEnter={() => console.log('enter')} />
-        <Icon name="alipay" onMouseLeave={() => console.log('leave')} />
-    </div>
-    , document.querySelector("#root"))
+export default Icon;
