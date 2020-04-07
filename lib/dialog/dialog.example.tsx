7703bfb4c110934e0c2e5ca8eb45374721456333
mozhenghong <1445107381@qq.com>
2020-04-07 18:03:39 +0800

    添加ReactDOM.createPortal 传送门

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,20 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, Fragment} from 'react'
 import Dialog from './dialog'
 
 export default function () {
     const [visible, setVisible] = useState(false)
+    const [visible1, setVisible1] = useState(false)
     return (
-        <div>
-            <button onClick={() => { setVisible(!visible) }}>click me!</button>
+        <Fragment>
+        <div style={{marginBottom: '10px'}}>
+            <button onClick={() => { setVisible(!visible) }}>点浮层不可关闭</button>
             <Dialog visible={visible}  closeOnClickMask = {false} onClose={() => {
                 setVisible(false)
             }}
                 buttons={
-                    [<button>确定</button>, <button>取消</button>]
+                    [<button onClick={() => { setVisible(false) }}>确定</button>, 
+                    <button onClick={() => { setVisible(false) }}>取消</button>]
                 }>
                 <div>
                     这是一个弹窗
                 </div>
             </Dialog>
-        </div>)
+        </div>
+         <div>
+         <button onClick={() => { setVisible1(!visible1) }}>点浮层可以关闭</button>
+         <Dialog visible={visible1}  closeOnClickMask = {true} onClose={() => {
+             setVisible1(false)
+         }}
+             buttons={
+                 [<button onClick={() => { setVisible1(false) }}>确定</button>, 
+                 <button onClick={() => { setVisible1(false) }}>取消</button>]
+             }>
+             <div>
+                 这是一个弹窗
+             </div>
+         </Dialog>
+     </div>
+     </Fragment>
+        )
 }
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,4 +1,5 @@
 import React, {Fragment, ReactElement} from 'react'
+import ReactDOM from 'react-dom'
 import Icon from '../icon/icon'
 import './dialog.scss'
 import {scopedClassMaker} from '../classes'
@@ -22,28 +23,30 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
             props.onClose(e)
         }
     }
+    const element  =   props.visible ?
+    <Fragment>
+        <div className={sc('mask')} onClick={onClickMask}></div>
+        <div className={sc()}>
+            <div className={sc('close')} onClick={onClickClose}>
+                <Icon name="close"/>
+            </div>
+            <header className={sc('header')}>
+                弹窗
+            </header>
+            <main className={sc('main')}>
+                {props.children}
+            </main>
+            <footer className={sc('footer')}>
+               {props.buttons.map((button,index) => {
+                   return React.cloneElement(button, {key: index})
+               })}
+            </footer>
+        </div>
+    </Fragment> :
+     null
     return (
-        props.visible ?
-           <Fragment>
-               <div className={sc('mask')} onClick={onClickMask}></div>
-               <div className={sc()}>
-                   <div className={sc('close')} onClick={onClickClose}>
-                       <Icon name="close"/>
-                   </div>
-                   <header className={sc('header')}>
-                       弹窗
-                   </header>
-                   <main className={sc('main')}>
-                       {props.children}
-                   </main>
-                   <footer className={sc('footer')}>
-                      {props.buttons.map((button,index) => {
-                          return React.cloneElement(button, {key: index})
-                      })}
-                   </footer>
-               </div>
-           </Fragment> :
-            null
+    // react传送门
+      ReactDOM.createPortal(element, document.body)
     )
 }
 
