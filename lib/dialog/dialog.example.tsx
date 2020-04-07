cf44e236b5257a7d291ada69624ff3ac036e46f0
mozhenghong <1445107381@qq.com>
2020-04-07 17:45:10 +0800

    dialog添加点击事件

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -5,8 +5,13 @@ export default function () {
     const [visible, setVisible] = useState(false)
     return (
         <div>
-            <button onClick={() => {setVisible(!visible)}}>click me!</button>
-            <Dialog visible = {visible}>
+            <button onClick={() => { setVisible(!visible) }}>click me!</button>
+            <Dialog visible={visible}  closeOnClickMask = {false} onClose={() => {
+                setVisible(false)
+            }}
+                buttons={
+                    [<button>确定</button>, <button>取消</button>]
+                }>
                 <div>
                     这是一个弹窗
                 </div>
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,22 +1,33 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, ReactElement} from 'react'
 import Icon from '../icon/icon'
 import './dialog.scss'
 import {scopedClassMaker} from '../classes'
 
 interface Props {
-    visible: boolean
+    visible: boolean;
+    buttons: Array<ReactElement>;
+    onClose: React.MouseEventHandler;
+    closeOnClickMask?: boolean;
 }
 
 const scopedClass = scopedClassMaker('moui-dialog')
 const sc = scopedClass
 
 const Dialog: React.FunctionComponent<Props> = (props) => {
+    const onClickClose:React.MouseEventHandler = (e) => {
+        props.onClose(e)
+    }
+    const onClickMask:React.MouseEventHandler = (e) => {
+        if(props.closeOnClickMask) {
+            props.onClose(e)
+        }
+    }
     return (
         props.visible ?
            <Fragment>
-               <div className={sc('mask')}></div>
+               <div className={sc('mask')} onClick={onClickMask}></div>
                <div className={sc()}>
-                   <div className={sc('close')}>
+                   <div className={sc('close')} onClick={onClickClose}>
                        <Icon name="close"/>
                    </div>
                    <header className={sc('header')}>
@@ -26,8 +37,9 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
                        {props.children}
                    </main>
                    <footer className={sc('footer')}>
-                       <button>确定</button>
-                       <button>取消</button>
+                      {props.buttons.map((button,index) => {
+                          return React.cloneElement(button, {key: index})
+                      })}
                    </footer>
                </div>
            </Fragment> :
@@ -35,4 +47,7 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
     )
 }
 
+Dialog.defaultProps = {
+    closeOnClickMask: false
+}
 export default Dialog
