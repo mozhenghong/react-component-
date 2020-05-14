706e7f21de28113ae02ecd1bf0f9bd7daa429f1b
momo <1445107381@qq.com>
2020-05-14 18:08:55 +0800

    优化demo代码高亮显示

--- demo.tsx
+++ demo.tsx
@@ -1,15 +1,34 @@
 import * as React from 'react'
+import Highlight, { defaultProps } from 'prism-react-renderer'
 
 interface Props {
     code: string
 }
 const Demo: React.FunctionComponent<Props> = (props) => {
+    const [codevisible, setCodevisible] = React.useState(false)
     return (
         <div>
-            {props.children}
-            <pre>
-                {props.code}
-            </pre>
+            <div>
+                {props.children}
+            </div>
+            <div>
+                <button onClick={() => { setCodevisible(!codevisible) }}>查看代码</button>
+            </div>
+            {codevisible &&
+                <Highlight {...defaultProps} code={props.code} language="jsx">
+                    {({ className, style, tokens, getLineProps, getTokenProps }) => (
+                        <pre className={className} style={style}>
+                            {tokens.map((line, i) => (
+                                <div {...getLineProps({ line, key: i })}>
+                                    {line.map((token, key) => (
+                                        <span {...getTokenProps({ token, key })} />
+                                    ))}
+                                </div>
+                            ))}
+                        </pre>
+                    )}
+                </Highlight>
+            }
         </div>
     )
 }
