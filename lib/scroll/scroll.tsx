c23ac54e9ad5ecccfc832ec672aba2ab1af0a9d3
momo <1445107381@qq.com>
2020-08-13 16:17:03 +0800

    scroll组件创建滚动条

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,5 +1,6 @@
-import React, { HTMLAttributes} from 'react'
+import React, { HTMLAttributes, UIEventHandler, useState,useEffect,useRef} from 'react'
 import './scroll.scss'
+import scrollbarWidth from './scrollbar-width'
 
 interface Props extends HTMLAttributes<HTMLDivElement>{
 
@@ -7,10 +8,29 @@ interface Props extends HTMLAttributes<HTMLDivElement>{
 
 const Scroll:React.FunctionComponent<Props> = (props) => {
     const {children, ...rest} = props
+    const [barHeight,setBarHeight]=useState(0)
+    const [barTop, setBarTop] = useState(0)
+    const containerRef = useRef<HTMLDivElement>(null)
+    const onScroll: UIEventHandler = (e) =>{
+        const scrollHeight = containerRef.current!.scrollHeight
+        const viewHeight = containerRef.current!.getBoundingClientRect().height
+        const scrollTop = containerRef.current!.scrollTop
+        setBarTop(scrollTop*viewHeight/scrollHeight)
+    }
+    useEffect(() => {
+        const scrollHeight = containerRef.current!.scrollHeight
+        const viewHeight = containerRef.current!.getBoundingClientRect().height
+        setBarHeight(viewHeight*viewHeight/scrollHeight)
+    },[])
     return (
         <div className="moui-scroll" {...rest}>
-            <div className="moui-scroll-inner">
+            <div className="moui-scroll-inner" style={{right:-scrollbarWidth()}}
+                ref={containerRef}
+                onScroll={onScroll}>
                 {children}
+            </div>
+            <div className="moui-scroll-track">
+                <div className="moui-scroll-bar" style={{height:barHeight, transform: `translateY(${barTop}px)`}}></div>
             </div>
         </div>
     )
