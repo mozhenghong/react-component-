eed1bfb93d72b866e9ab992ba24d9510e58c2547
momo <1445107381@qq.com>
2020-08-19 16:00:02 +0800

    递归渲染+样式

--- lib/tree/tree.example.tsx
+++ lib/tree/tree.example.tsx
@@ -1,55 +1,41 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Tree from './tree'
 
 const TreeExample:React.FunctionComponent = () => {
-    const [treeData, setTreeData] = useState([{
-        text: '一',
+    const [treeData] = useState([{
+        text: '1',
         value: '1',
         children: [{
-            text: '一一',
-            value: '1.1'
+            text: '1.1',
+            value: '1.1',
+            children: [{
+                text: '1.1.1',
+                value: '1.1.1'
+            },{
+                text: '1.2.1',
+                value: '1.2.1'
+            }]
         },{
-            text: '一二',
+            text: '1.2',
             value: '1.2'
         }]
     },{
-        text: '二',
+        text: '2',
         value: '2',
         children: [{
-            text: '二一',
+            text: '2.1',
             value: '2.1'
         },{
-            text: '二二',
+            text: '2.2',
             value: '2.2'
         }]
     }])
-    useEffect(() => {
-        setTreeData([{
-            text: '一',
-            value: '1',
-            children: [{
-                text: '一一',
-                value: '1.1'
-            },{
-                text: '一二',
-                value: '1.2'
-            }]
-        },{
-            text: '二',
-            value: '2',
-            children: [{
-                text: '二一',
-                value: '2.1'
-            },{
-                text: '二二',
-                value: '2.2'
-            }]
-        }])
-    },[])
     return (
         <div>
             <h3>展示数据</h3>
-            <Tree sourceData={treeData}/>
+            <div style={{width: '200px'}}>
+                <Tree sourceData={treeData}/>
+            </div>
         </div>
     )
 }
--- lib/tree/tree.tsx
+++ lib/tree/tree.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {scopedClassMaker} from '../helpers/classes'
+import './tree.scss'
 
 interface SourceDataItem {
     text: string;
@@ -9,15 +11,28 @@ interface Props {
     sourceData: SourceDataItem[]
 }
 
-const Tree:React.FunctionComponent<Props> = (props) => {
+const scopedClass = scopedClassMaker('moui-tree')
+const sc = scopedClass
+
+//递归渲染数据
+const renderItem = (item: SourceDataItem, level=1) => {
+    const classes = {
+        ['level-' + level]: true,
+        'item':true
+    }
+    return <div key={item.value} className={sc(classes)}>
+        <div className={sc('text')}>{item.text}</div>
+        {item.children?.map((inneritem) => {
+            return renderItem(inneritem, level+1)
+        })}
+    </div>
+}
+
+const Tree: React.FunctionComponent<Props> = (props) => {
     return (
         <div>
             {props.sourceData.map((item) => {
-                return <div>{item.text}
-                    {item.children &&  item.children.map((inneritem) => {
-                        return<div>{inneritem.text}</div>
-                    })}
-                </div>
+                return renderItem(item)
             })}
         </div>
     )
